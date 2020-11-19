fdf96d702559d73c37be9dc84b0e014609105d11
Lucas Homer-Contractor <lucas.homer@selectquote.com>
2020-11-19 07:57:55 -0800

    feat: 🎸 add SQFormCheckboxGroup and SQFormCheckboxGroupItem
    SQFormCheckboxGroup and -Item comps enable an 'inclusion list' UX to
    add/remove items from a larger list of options via checkbox, with an
    optional 'select all' functionality built in

---
+++ src/components/SQForm/SQFormCheckboxGroup.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Grid} from '@material-ui/core';
+import {
+  SQFieldArray,
+  SQFormCheckboxGroupItem,
+  useSQFormContext
+} from '../../../src';
+
+export default function SQFormCheckboxGroup({
+  name,
+  children,
+  useSelectAll = false,
+  selectAllData = null,
+  selectAllContainerProps = {},
+  selectAllProps = {}
+}) {
+  const {values, setFieldValue} = useSQFormContext();
+
+  if (!useSelectAll) {
+    return <SQFieldArray name={name}>{children}</SQFieldArray>;
+  }
+
+  return (
+    <>
+      <Grid container {...selectAllContainerProps}>
+        <SQFormCheckboxGroupItem
+          name="selectAll"
+          label="Select All"
+          isChecked={values.selectAll}
+          onChange={e => {
+            if (e.target.checked) {
+              setFieldValue(name, selectAllData);
+            } else {
+              setFieldValue(name, []);
+            }
+            setFieldValue('selectAll', !values.selectAll);
+          }}
+          {...selectAllProps}
+        />
+      </Grid>
+      <SQFieldArray name={name}>{children}</SQFieldArray>
+    </>
+  );
+}
+
+SQFormCheckboxGroup.propTypes = {
+  /** the `name` must match the name of the desired array in `initialValues` */
+  name: PropTypes.string.isRequired,
+  /** the `children` must be a single SQFormCheckboxGroupItem or an array of them */
+  children: PropTypes.oneOfType([
+    PropTypes.element,
+    PropTypes.elementType,
+    PropTypes.arrayOf([PropTypes.element, PropTypes.elementType])
+  ]).isRequired
+};
