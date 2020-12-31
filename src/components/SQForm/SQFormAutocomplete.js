6825df890ee4cff86284c9110112e68d0fdbfc2d
Chris Boyle <chris.boyle@selectquote.com>
2020-12-30 19:41:46 -0600

    fix: 🐛 Allow default value to be used in autocomplete component
    ✅ Closes: #43

--- src/components/SQForm/SQFormAutocomplete.js
+++ src/components/SQForm/SQFormAutocomplete.js
@@ -102,6 +102,17 @@ function SQFormAutocomplete({
     name,
     isRequired
   });
+
+  const initialValue = children.find(option => {
+    if (option.value === value) {
+      if (!touched.hasOwnProperty(name)) {
+        setTouched({...touched, ...{[name]: true}});
+      }
+      return option;
+    }
+    return null;
+  });
+
   const [inputValue, setInputValue] = React.useState('');
   const prevValue = usePrevious(value);
 
@@ -150,6 +161,7 @@ function SQFormAutocomplete({
         onChange={handleAutocompleteChange}
         onInputChange={handleInputChange}
         inputValue={inputValue}
+        value={initialValue}
         getOptionLabel={option => option.label}
         renderInput={params => {
           return (
