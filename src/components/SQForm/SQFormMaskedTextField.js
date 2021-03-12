c3946e20591d7ec422b30a924f6ce0efc625ea9d
Cody Rose <cody.rose@selectquote.com>
2021-03-11 20:34:38 -0600

    refactor: 💡 add proptypes for text field and correct mask types

--- src/components/SQForm/SQFormMaskedTextField.js
+++ src/components/SQForm/SQFormMaskedTextField.js
@@ -62,7 +62,35 @@ function SQFormMaskedTextField({
 
 SQFormMaskedTextField.propTypes = {
   /** Valid mask array; custom or from utils/masks.js */
-  mask: PropTypes.array.isRequired
+  mask: PropTypes.oneOfType([PropTypes.array, PropTypes.func]),
+  /** Name of the field will be the Object key of the key/value pair form payload */
+  name: PropTypes.string.isRequired,
+  /** Descriptive label of the input */
+  label: PropTypes.string.isRequired,
+  /** Placeholder text used inside the input field to provide hints to the user */
+  placeholder: PropTypes.string,
+  /** Disabled property to disable the input if true */
+  isDisabled: PropTypes.bool,
+  /** Required property used to highlight input and label if not fulfilled */
+  isRequired: PropTypes.bool,
+  /** Size of the input given full-width is 12. */
+  size: PropTypes.oneOf(['auto', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+  /** Custom onBlur event callback */
+  onBlur: PropTypes.func,
+  /** Custom onChange event callback */
+  onChange: PropTypes.func,
+  /** Adornment that appears at the start of the input */
+  startAdornment: PropTypes.node,
+  /** Adornment that appears at the end of the input */
+  endAdornment: PropTypes.node,
+  /** Defines the input type for the text field. Must be a valid HTML5 input type */
+  type: PropTypes.string,
+  /** Props applied to the Input element */
+  InputProps: PropTypes.object,
+  /** Attributes applied to the `input` element */
+  inputProps: PropTypes.object,
+  /** Any valid prop for material ui text input child component - https://material-ui.com/api/text-field/#props */
+  muiFieldProps: PropTypes.object
 };
 
 export default SQFormMaskedTextField;
