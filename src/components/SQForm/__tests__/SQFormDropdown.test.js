d856746931d7e7aebcb99b905a6adbccd7cb1681
Laurel Bean <laurel.bean@selectquote.com>
2021-03-30 11:24:03 -0500

    test: 💍 Adding more tests to SQFormDropdown
    Adding more tests to SQFormDropdown
    
    ✅ Closes: #168

--- src/components/SQForm/__tests__/SQFormDropdown.test.js
+++ src/components/SQForm/__tests__/SQFormDropdown.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {screen} from '@testing-library/react';
 import SQFormDropdown from '../SQFormDropdown';
 import {renderSQForm} from '../../../utils/tests';
+import userEvent from '@testing-library/user-event';
 
 const defaultProps = {
   name: 'dropdownExample',
@@ -11,6 +12,30 @@ const defaultProps = {
 const createDropdown = (options, fieldProps = {}) => {
   return <SQFormDropdown {...fieldProps}>{options}</SQFormDropdown>;
 };
+
+it('should render list of options', () => {
+  const initialValues = {
+    dropdownExample: ''
+  };
+
+  const options = [
+    {label: 'Test 1', value: 1},
+    {label: 'Test 2', value: 2},
+    {label: 'Test 3', value: 3}
+  ];
+
+  const renderDropdown = createDropdown(options, defaultProps);
+
+  renderSQForm(renderDropdown, {initialValues});
+
+  const expandButton = screen.getByRole('button', /dropdown example/i);
+  userEvent.click(expandButton);
+
+  const optionsList = screen.getByRole('listbox', /dropdown example/i);
+
+  expect(optionsList).toBeInTheDocument();
+  expect(optionsList.children.length).toBe(3);
+});
 
 it('should render with initial value', () => {
   const initialValues = {
@@ -24,10 +49,58 @@ it('should render with initial value', () => {
 
   renderSQForm(createDropdown(options, defaultProps), {initialValues});
 
-  //The input DOM object has 'aria-hidden'=true set so the Testing Library can't find it
-  //That's why currently getting it via parent/child nodes
   const labelValue = screen.getByRole('button', /dropdown example/i);
-  const input = labelValue.parentNode.childNodes[1];
+  expect(labelValue).toHaveTextContent('Option 2');
+});
 
-  expect(input).toHaveValue('2');
+it('should disable option', () => {
+  const initialValues = {
+    dropdownExample: ''
+  };
+
+  const options = [
+    {label: 'Test 1', value: 1},
+    {label: 'Test 2', value: 2, isDisabled: true},
+    {label: 'Test 3', value: 3}
+  ];
+
+  const renderDropdown = createDropdown(options, defaultProps);
+
+  renderSQForm(renderDropdown, {initialValues});
+
+  const expandButton = screen.getByRole('button', /dropdown example/i);
+  userEvent.click(expandButton);
+
+  const optionsList = screen.getByRole('listbox', /dropdown example/i);
+
+  expect(optionsList.childNodes[1]).toHaveAttribute('aria-disabled', 'true');
+});
+
+it('should display an empty option when displayEmpty is true', () => {
+  const initialValues = {
+    dropdownExample: ''
+  };
+
+  const fieldProps = {
+    ...defaultProps,
+    displayEmpty: true
+  };
+
+  const options = [
+    {label: 'Test 1', value: 1},
+    {label: 'Test 2', value: 2},
+    {label: 'Test 3', value: 3}
+  ];
+
+  const renderDropdown = createDropdown(options, fieldProps);
+
+  renderSQForm(renderDropdown, {initialValues});
+
+  const expandButton = screen.getByRole('button', /dropdown example/i);
+  userEvent.click(expandButton);
+
+  const optionsList = screen.getByRole('listbox', /dropdown example/i);
+
+  expect(optionsList.children.length).toBe(4);
+  expect(optionsList.childNodes[0]).toHaveTextContent('- -');
 });
