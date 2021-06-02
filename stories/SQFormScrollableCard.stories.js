c23621d31ca1f86cb55db59d37ef3c0cc84c5424
Grant Doohen <grant.doohen@selectquote.com>
2021-06-02 10:09:29 -0700

    refactor: added more stories to showcase SubHeaderComponent, height, and isSelfBounding props

--- stories/SQFormScrollableCard.stories.js
+++ stories/SQFormScrollableCard.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as Yup from 'yup';
 
+import {Paper} from '@material-ui/core';
 import {SQFormScrollableCard, SQFormTextField} from '../src';
 import {createDocsPage} from './utils/createDocsPage';
 
@@ -28,108 +29,157 @@ const defaultArgs = {
   validationSchema: {hello: Yup.string().required('Required')}
 };
 
-const Template = args => {
-  return (
-    <SQFormScrollableCard {...args}>
+const Template = (args = {}) => {
+  const {wrapper, validationSchema, ...restArgs} = args;
+
+  const basicCard = (
+    <SQFormScrollableCard {...restArgs}>
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
       <SQFormTextField
         name="hello"
         label="Hello"
         size={12}
-        isRequired={Boolean(args.validationSchema)}
+        isRequired={Boolean(validationSchema)}
       />
     </SQFormScrollableCard>
+  );
+
+  return (
+    <>{wrapper ? React.cloneElement(wrapper, {}, basicCard) : basicCard}</>
   );
 };
 
 export const Default = Template.bind({});
-Default.args = defaultArgs;
+Default.args = {
+  ...defaultArgs
+};
+
+const SubHeader = () => (
+  <Paper elevation={2}>
+    <h3 style={{textAlign: 'center', padding: '10px'}}>Sub Header</h3>
+  </Paper>
+);
+
+export const WithSubHeader = Template.bind({});
+WithSubHeader.args = {
+  ...defaultArgs,
+  SubHeaderComponent: <SubHeader />
+};
+
+const Wrapper = ({children}) => (
+  <div style={{height: '100%', overflow: 'hidden'}}>
+    <Paper
+      elevation={2}
+      style={{height: '30%', margin: '10px 0', textAlign: 'center'}}
+    >
+      <h2>Imaginary beatiful content</h2>
+    </Paper>
+    {children}
+  </div>
+);
+
+export const WithSelfBoundingHeight = Template.bind({});
+WithSelfBoundingHeight.args = {
+  ...defaultArgs,
+  title: 'With Self-Bounding Height',
+  isSelfBounding: true,
+  wrapper: <Wrapper />,
+  SubHeaderComponent: <SubHeader />
+};
+
+export const WithStaticHeight = Template.bind({});
+WithStaticHeight.args = {
+  ...defaultArgs,
+  title: 'With Static Height',
+  height: 450,
+  SubHeaderComponent: <SubHeader />
+};
