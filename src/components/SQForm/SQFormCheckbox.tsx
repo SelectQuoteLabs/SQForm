0f1eec0ee0c2658254aaebc718a846ca9d83fddd
Caleb O'Leary <caleb.oleary@selectquote.com>
2021-06-28 12:03:18 -0500

    refactor: 💡 Convert SQFormCheckbox to typescript Ref #244
    ✅ Closes: #314

--- src/components/SQForm/SQFormCheckbox.tsx
+++ src/components/SQForm/SQFormCheckbox.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Checkbox from '@material-ui/core/Checkbox';
+import {Checkbox, CheckboxProps} from '@material-ui/core';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Grid from '@material-ui/core/Grid';
 
 import {useForm} from './useForm';
+
+interface SQFormCheckboxProps {
+  /** Disabled state of the checkbox */
+  isDisabled?: boolean;
+  /** Descriptive label text for the checkbox */
+  label: string;
+  /** Unique name of the checkbox element */
+  name: string;
+  /** Custom onChange event callback */
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  /** Size of the input given full-width is 12. */
+  size?: 'auto' | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  /** Any valid prop for material ui checkbox child component - https://material-ui.com/api/checkbox/#props */
+  muiFieldProps?: CheckboxProps;
+}
 
 function SQFormCheckbox({
   isDisabled = false,
@@ -13,7 +27,7 @@ function SQFormCheckbox({
   onChange,
   size = 'auto',
   muiFieldProps = {}
-}) {
+}: SQFormCheckboxProps): JSX.Element {
   const {
     formikField: {field},
     fieldHelpers: {handleChange}
@@ -28,7 +42,7 @@ function SQFormCheckbox({
       <FormControlLabel
         control={
           <Checkbox
-            checked={field.value}
+            checked={field.value as boolean}
             color="primary"
             disabled={isDisabled}
             name={name}
@@ -41,20 +55,5 @@ function SQFormCheckbox({
     </Grid>
   );
 }
-
-SQFormCheckbox.propTypes = {
-  /** Disabled state of the checkbox */
-  isDisabled: PropTypes.bool,
-  /** Descriptive label text for the checkbox */
-  label: PropTypes.string.isRequired,
-  /** Unique name of the checkbox element */
-  name: PropTypes.string.isRequired,
-  /** Custom onChange event callback */
-  onChange: PropTypes.func,
-  /** Size of the input given full-width is 12. */
-  size: PropTypes.oneOf(['auto', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
-  /** Any valid prop for material ui checkbox child component - https://material-ui.com/api/checkbox/#props */
-  muiFieldProps: PropTypes.object
-};
 
 export default SQFormCheckbox;
