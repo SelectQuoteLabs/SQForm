8daaedcd84ecb4dff7a7940964ca1ad57043ee0c
ishaqk <ishaqk@devnatives.com>
2021-08-13 16:21:06 +0500

    refactor: 💡 Convert useInitialRequiredErrors to typescript Ref #244

--- src/hooks/useInitialRequiredErrors.tsx
+++ src/hooks/useInitialRequiredErrors.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import {FormikErrors} from 'formik';
+
+type ValidationSchema = {
+  [key: string]: unknown;
+};
 
 // Until Formik exposes the validationSchema (again) via Context, the solution has to be handled at the Form declaration level
 // There's a few open PR's on this issue, here's one for reference: https://github.com/formium/formik/pull/2933
-export function useInitialRequiredErrors(validationSchema = {}) {
+export function useInitialRequiredErrors(
+  validationSchema: ValidationSchema = {}
+): FormikErrors<unknown> {
   const initialRequiredErrorsRef = React.useRef(
     Object.entries(validationSchema).reduce((acc, [key, value]) => {
       if (value._exclusive?.required) {
