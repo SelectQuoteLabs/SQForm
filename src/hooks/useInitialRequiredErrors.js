a9747fbc9962591c7c8cbc5990cb2ecece6d2b1a
Sean Groff <sean.groff@selectquote.com>
2021-10-07 16:19:50 -0500

    fix: 🐛 Only shows green fulfilled icon if field updated by user
    ✅ Closes: #459

--- src/hooks/useInitialRequiredErrors.js
+++ src/hooks/useInitialRequiredErrors.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-// Until Formik exposes the validationSchema (again) via Context, the solution has to be handled at the Form declaration level
+function _getHasValue(fieldValue) {
+  if (Array.isArray(fieldValue)) {
+    return !!fieldValue.length;
+  }
+
+  if (typeof fieldValue === 'number') {
+    return true;
+  }
+
+  if (typeof fieldValue === 'boolean') {
+    return true;
+  }
+
+  return !!fieldValue;
+}
+
+// Until Formik exposes the validationSchema (again) via useFormikContext(), the solution has to be handled at the Form declaration level
 // There's a few open PR's on this issue, here's one for reference: https://github.com/formium/formik/pull/2933
-export function useInitialRequiredErrors(validationSchema = {}) {
+export function useInitialRequiredErrors(validationSchema = {}, initialValues) {
   const initialRequiredErrorsRef = React.useRef(
     Object.entries(validationSchema).reduce((acc, [key, value]) => {
-      if (value._exclusive?.required) {
+      if (value._exclusive?.required && !_getHasValue(initialValues[key])) {
         return {...acc, [key]: 'Required'};
       }
       return acc;
