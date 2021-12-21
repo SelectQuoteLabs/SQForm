6c0a681b51949cf120c13b4c40f719337de827a2
Aaron Harpt <aaron.harpt@selectquote.com>
2021-12-21 12:36:11 -0600

    refactor: 💡 Migrate SQFormMaskedReadOnlyField to TypeScript
    ✅ Closes: #474

--- src/components/SQForm/SQFormMaskedReadOnlyField.tsx
+++ src/components/SQForm/SQFormMaskedReadOnlyField.tsx
@@ -14,8 +14,14 @@ interface TextFieldMaskProps extends React.HTMLAttributes<HTMLInputElement> {
 interface SQFormMaskedReadOnlyFieldProps extends SQFormReadOnlyFieldProps {
   /** Valid mask array; custom or from utils/masks.js */
   mask?: maskProp;
+  /** Name of the field will be the Object key of the key/value pair form payload */
+  name: string;
+  /** Descriptive label of the input */
+  label: string;
   /** Placeholder text used inside the input field to provide hints to the user */
   placeholder?: string;
+  /** Size of the input given full-width is 12. */
+  size?: 'auto' | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 }
 
 function TextFieldMask({
