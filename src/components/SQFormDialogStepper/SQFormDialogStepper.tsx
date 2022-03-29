9af48389ca8d275be56e8afad1149118964e2303
Harold Vilander <harold.vilander@selectquote.com>
2022-03-29 10:12:44 -0500

    fix: 🐛 resolves some type errors after merging in some changes

--- src/components/SQFormDialogStepper/SQFormDialogStepper.tsx
+++ src/components/SQFormDialogStepper/SQFormDialogStepper.tsx
@@ -116,7 +116,10 @@ export interface SQFormDialogStepperProps<Values extends FormikValues> {
   /** Determine the max-width of the dialog. The dialog width grows with the size of the screen. Set to false to disable maxWidth. */
   maxWidth?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | false;
   /** Callback function invoked when the user clicks on the secondary button or outside the Dialog */
-  onClose: () => void;
+  onClose: (
+    event: Record<string, unknown>,
+    reason: 'backdropClick' | 'escapeKeyDown' | 'cancelClick'
+  ) => void;
   /** Callback function invoked when user clicks primary submit button */
   onSubmit: (values: Values, helpers: FormikHelpers<Values>) => void;
   /** Title text at the top of the Dialog */
@@ -229,10 +232,10 @@ export function SQFormDialogStepper<Values extends FormikValues>({
   }
 
   const handleClose = (
-    event, // Record<string, unknown>,
-    reason // 'backdropClick' | 'escapeKeyDown' | 'cancelClick'
+    event: Record<string, unknown>,
+    reason: 'backdropClick' | 'escapeKeyDown' | 'cancelClick'
   ) => {
-    if (disableBackdropClick && reason === 'backDropClick') {
+    if (disableBackdropClick && reason === 'backdropClick') {
       return;
     }
 
