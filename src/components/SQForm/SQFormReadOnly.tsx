238f463c07886fc37022cb9eb054524df8b22870
Sean Groff <sean.groff@selectquote.com>
2022-04-08 15:56:10 -0500

    feat: 🎸 Read only form component
    ✅ Closes: #561

---
+++ src/components/SQForm/SQFormReadOnly.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import {Formik, Form} from 'formik';
+import SQFormReadOnlyField from './SQFormReadOnlyField';
+import {noop} from 'utils';
+import type {CSSProperties} from 'react';
+import type {GridProps} from '@material-ui/core';
+import type {FormikValues} from 'formik';
+import type {SQFormReadOnlyFieldProps} from './SQFormReadOnlyField';
+
+export interface SQFormReadOnlyProps<Values extends FormikValues> {
+  /** Form Input(s) */
+  readOnlyFields: SQFormReadOnlyFieldProps[];
+  /** Form Entity Object */
+  initialValues: Values;
+  /** Any prop from https://material-ui.com/api/grid */
+  muiGridProps?: GridProps;
+}
+
+const noBottomMargin: CSSProperties = {
+  marginBottom: 0,
+};
+const noBottomMarginStyle = {style: noBottomMargin};
+
+function SQFormReadOnly<Values extends FormikValues>({
+  readOnlyFields,
+  initialValues,
+  muiGridProps = {},
+}: SQFormReadOnlyProps<Values>): JSX.Element {
+  return (
+    <Formik<Values> initialValues={initialValues} onSubmit={noop}>
+      {(_props) => {
+        return (
+          <Form>
+            <Grid
+              {...muiGridProps}
+              container
+              spacing={muiGridProps.spacing ?? 2}
+            >
+              {readOnlyFields.map((readOnlyField, index) => {
+                return (
+                  <SQFormReadOnlyField
+                    key={index}
+                    muiFieldProps={noBottomMarginStyle}
+                    {...readOnlyField}
+                  />
+                );
+              })}
+            </Grid>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+}
+
+export default SQFormReadOnly;
---
+++ stories/__tests__/SQFormReadOnly.stories.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {composeStories} from '@storybook/testing-react';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import * as stories from '../SQFormReadOnly.stories';
+
+const {Default: SQFormReadOnly} = composeStories(stories);
+
+describe('SQFormReadOnly Tests', () => {
+  describe('Default ReadOnly Form', () => {
+    it('should render labels and read-only text fields', () => {
+      render(<SQFormReadOnly />);
+
+      const label = screen.getByText(/first name/i);
+      expect(label).toBeInTheDocument();
+      expect(label).toHaveTextContent('First Name');
+
+      const textField = screen.getByRole('textbox', {name: /firstName/i});
+      expect(textField).toBeInTheDocument();
+    });
+
+    it('should render with default "- -" initial values', () => {
+      render(<SQFormReadOnly />);
+
+      const textField = screen.getByRole('textbox', {name: /preferredName/i});
+      expect(textField).toHaveValue('- -');
+    });
+
+    it('should NOT update when text is entered', () => {
+      render(<SQFormReadOnly size="auto" />);
+
+      const textField = screen.getByRole('textbox', {name: /preferredName/i});
+      expect(textField).toHaveValue('- -');
+      userEvent.type(textField, 'test');
+      expect(textField).toHaveValue('- -');
+    });
+  });
+
+  describe('ReadOnly field with non-empty initial Value', () => {
+    it('should render with the custom initialValue provided', () => {
+      render(<SQFormReadOnly />);
+
+      const textField = screen.getByRole('textbox', {name: /firstName/i});
+      expect(textField).toHaveValue('Jane');
+    });
+  });
+});
