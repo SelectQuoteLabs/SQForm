92feb80a0cb0a93aa7dba6053223cff6a2958357
aharpt <aaron.harpt@gmail.com>
2022-08-15 17:04:12 -0500

    refactor: 💡 Migrate TopSubHeader to MUI5
    ✅ Closes: #772

--- src/components/SQFormScrollableCard/TopSubHeader.tsx
+++ src/components/SQFormScrollableCard/TopSubHeader.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import {Paper, makeStyles} from '@material-ui/core';
+import {Paper} from '@mui/material';
 
 type TopSubHeaderProps = {
   children: React.ReactNode;
 };
 
-const useStyles = makeStyles((theme) => {
-  return {
-    container: {
-      padding: `${theme.spacing(1.5)}px ${theme.spacing(2)}px`,
-      marginBottom: `${theme.spacing(4)}px`,
-    },
-  };
-});
-
 function TopSubHeader({children}: TopSubHeaderProps): React.ReactElement {
-  const classes = useStyles();
-
   return (
-    <Paper elevation={2} className={classes.container}>
+    <Paper
+      elevation={2}
+      sx={(theme) => ({
+        p: `${theme.spacing(1.5)} ${theme.spacing(2)}`,
+        mb: `${theme.spacing(4)}`,
+      })}
+    >
       {children}
     </Paper>
   );
